1e1d61c7af766af288188ed55926d11c1673b1de
Mel Florance <mel.florance@gmail.com>
2015-08-13 16:30:49 +0200

    Add keyboard movement and shoot for snipers on click on direction of the mouse

--- Game/Game.js
+++ Game/Game.js
@@ -19,19 +19,17 @@ Game.prototype.input = function()
     {
         if(callback.type == 'keyboard')
         {
-            switch (callback.event)
+            switch (callback.event.keyCode)
             {
-                case 90: Game.bindAction(callback.event, 'moveForward');  break;
-                case 83: Game.bindAction(callback.event, 'moveBackward'); break;
-                case 81: Game.bindAction(callback.event, 'moveLeft');     break;
-                case 68: Game.bindAction(callback.event, 'moveRight');    break;
+                case 90: Game.bindAction(callback.event.keyCode, 'moveForward');  break;
+                case 83: Game.bindAction(callback.event.keyCode, 'moveBackward'); break;
+                case 81: Game.bindAction(callback.event.keyCode, 'moveLeft');     break;
+                case 68: Game.bindAction(callback.event.keyCode, 'moveRight');    break;
             }
         }
 
-        if(callback.type == 'mouse')
-        {
-
-        }
+        if(callback.type == 'mouse:down')
+            Game.bindAction(callback.event, 'mouse:down');
     });
 };
 
@@ -99,7 +97,88 @@ Game.prototype.attack = function(player, enemy)
 
 Game.prototype.bindAction = function(key, action)
 {
-    console.log(key, action);
+    for(var x in Engine.objects)
+    {
+        if(Engine.objects[x].instance.type == 'player'
+        && Engine.objects[x].instance.role == 'sniper'
+        && Engine.objects[x].instance.team == 'red')
+        {
+            switch(action)
+            {
+                case 'moveForward' : Engine.objects[x].instance.vy -= Engine.objects[x].instance.speed +3; break;
+                case 'moveBackward': Engine.objects[x].instance.vy = Engine.objects[x].instance.speed +3; break;
+                case 'moveLeft'    : Engine.objects[x].instance.vx -= Engine.objects[x].instance.speed +3; break;
+                case 'moveRight'   : Engine.objects[x].instance.vx = Engine.objects[x].instance.speed +3; break;
+
+                case 'mouse:down':
+
+                    var f = 0;
+                    var rect = window.document.body.getBoundingClientRect();
+
+                    var x1 = Engine.objects[x].instance.cx;
+                    var y1 = Engine.objects[x].instance.cy;
+                    var x2 = key.clientX - rect.left;
+                    var y2 = key.clientY - rect.top;
+
+                    //Engine.ctx.save();
+                    //Engine.ctx.beginPath();
+                    //Engine.ctx.moveTo(x1,y1);
+                    //Engine.ctx.lineTo(x2, y2);
+                    //Engine.ctx.strokeStyle = 'red';
+                    //Engine.ctx.strokeWidth = '2';
+                    //Engine.ctx.stroke();
+                    //Engine.ctx.closePath();
+                    //Engine.ctx.restore();
+
+                    var vx, vy;
+                    var dx = x2 - x1,
+                        dy = y2 - y1;
+
+                    var dist  = Math.abs(Math.sqrt(dx * dx + dy * dy));
+                    var speed = Engine.fps_count / 1000 / dist;
+                    f += speed;
+
+                    angle = Math.atan(parseFloat(x2) / parseFloat(y2));
+
+                    vx = 4 * dx / Math.cos(angle) / 900;
+                    vy = 4 * dy / Math.sin(angle) / 900;
+
+                    px = x1 + (x2 - x1) * f;
+                    py = y1 + (y1 - y1) * f;
+
+
+                    if(x2 > Engine.objects[x].instance.cx)
+                        directionX = px + Engine.objects[x].instance.r + 10
+                    else
+                        directionX = px - Engine.objects[x].instance.r - 10
+
+
+                    if(y2 > Engine.objects[x].instance.cy)
+                        directionY = py + Engine.objects[x].instance.r + 10
+                    else
+                        directionY = py - Engine.objects[x].instance.r - 10
+
+                    var bullet = new Projectile(Engine.canvas, directionX, directionY, 3, 0, 0, false, vx, vy, 300, true, 0);
+                    bullet.type = 'projectile';
+                    bullet.r = 3;
+
+                    Engine.bindObject(bullet, {color:'red' , r: 3});
+
+
+                    break;
+            }
+
+            if(Engine.objects[x].instance.vx > Engine.objects[x].instance.maxSpeed)
+                Engine.objects[x].instance.vx = Engine.objects[x].instance.maxSpeed;
+            else if(Engine.objects[x].instance.vx < -Engine.objects[x].instance.maxSpeed)
+                Engine.objects[x].instance.vx = -Engine.objects[x].instance.maxSpeed;
+
+            if(Engine.objects[x].instance.vy > Engine.objects[x].instance.maxSpeed)
+                Engine.objects[x].instance.vy = Engine.objects[x].instance.maxSpeed;
+            else if(Engine.objects[x].instance.vy < -Engine.objects[x].instance.maxSpeed)
+                Engine.objects[x].instance.vy = -Engine.objects[x].instance.maxSpeed;
+        }
+    }
 };
 
 
@@ -117,15 +196,18 @@ Game.prototype.findFood = function(object)
                 // hit sensor
                 if(d < (object.r + Engine.objects[i].instance.r) + object.sensor)
                 {
-                    var dx = Engine.objects[i].instance.cx - object.cx;
-                    var dy = Engine.objects[i].instance.cy - object.cy;
+                    if(object.freemove)
+                    {
+                        var dx = Engine.objects[i].instance.cx - object.cx;
+                        var dy = Engine.objects[i].instance.cy - object.cy;
 
-                    object.vx = incrementX = dx / 30 + .1;
-                    object.vy = incrementY = dy / 30 + .1;
+                        object.vx = incrementX = dx / 30 + .1;
+                        object.vy = incrementY = dy / 30 + .1;
+                    }
 
-                    Engine.alpha += (Engine.delta / 10);
+                    Engine.alpha += (Engine.delta / 24);
                     if (Engine.alpha <= 0 || Engine.alpha >= 1) Engine.delta = -Engine.delta;
-                
+
                     Engine.ctx.save();
                     Engine.ctx.beginPath();
                     Engine.ctx.arc(object.cx, object.cy, object.sensor, object.sa, object.ea, object.c);
@@ -149,7 +231,6 @@ Game.prototype.findFood = function(object)
                         if(Engine.objects[i].instance.changeGravity)
                             Engine.physics.animateGravity(0.981);
 
-
                         object.health += Math.round(Math.random()) * 25 + 5;
                         object.r++;
                         object.force += 1;
@@ -162,14 +243,10 @@ Game.prototype.findFood = function(object)
 
 
                         this.deleteFood(Engine.objects[i].instance);
-                        object.freemove = true;
-                        this.freeMove(object);
+
+                        if(object.freemove)
+                            this.freeMove(object);
                     }
-                }
-                else
-                {
-                    object.freemove = true;
-                    this.freeMove(object);
                 }
             }
         }
@@ -187,13 +264,13 @@ Game.prototype.freeMove = function(object)
 {
     if(!object.freemove)
     {
-        Physics.randomVelocity(object);
-        setInterval(function()
-        {
-            Physics.randomVelocity(object);
-        }, 5000);
-
-        object.freemove = true;
+        //Physics.randomVelocity(object);
+        //setInterval(function()
+        //{
+        //    Physics.randomVelocity(object);
+        //}, 5000);
+        //
+        //object.freemove = true;
     }
 };
 
